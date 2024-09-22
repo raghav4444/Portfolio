d9a29e87959d0c19bb2aaca1f79172becb353ef7
raghav4444 <2023bcs065@axiscolleges.in>
2024-09-22 11:29:36 +0530

    1

--- js/script.js
+++ js/script.js
@@ -17,3 +17,30 @@ links.forEach((link, i) => {
     }
   });
 });
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".home-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
